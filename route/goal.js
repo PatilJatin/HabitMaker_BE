500f9de57cfad8d1f738afa9d3660bd4b0a9c2a8
Satan Sharma <107472942+satan7549@users.noreply.github.com>
2023-06-23 11:39:02 +0530

    Update goal.js

--- route/goal.js
+++ route/goal.js
@@ -10,10 +10,10 @@ import {
 } from "../controller/goalController.js";
 const router = express.Router();
 
-router.route("/update/task").post(isLoggedIn, createGoal);
+router.route("/create/task").post(isLoggedIn, createGoal);
 router.route("/delete/goal/:id").delete(isLoggedIn, deleteGoal);
 router.route("/get/allgoal").get(isLoggedIn, getAllGoals);
-router.route("/update/status/task").post(isLoggedIn,updateTaskStatus )
+router.route("/update/status/task").post(isLoggedIn, updateTaskStatus);
 router
   .route("/goal/:id")
   .get(isLoggedIn, getSingleGoal)
